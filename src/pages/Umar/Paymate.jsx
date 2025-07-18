0aad419f941c84b0da08b211450dba0238d1d772
SharifzodaUmar <SharifzodaUmar>
2025-07-18 17:10:58 +0000

    add

--- src/pages/Umar/Paymate.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Paymate = () => {
-  return (
-	 <div>Paymate</div>
-  )
-}
-
-export default Paymate
--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { todoSlice } from '../reducers/todoListSlice.jsx'
-export const store = configureStore({
-	reducer: {
-		todolist: todoSlice.reducer,
-	},
-})
