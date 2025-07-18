f28e0b7e112a8efc7a9cd2bd64a71b89d23abe77
SharifzodaUmar <SharifzodaUmar>
2025-07-18 17:03:22 +0000

    uhuhj

---
+++ my-app/src/pages/Umar/Contact.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import s1 from './suratoUmar/s1.png'
+import s2 from './suratoUmar/s2.png'
+import s3 from './suratoUmar/s3.png'
+import s4 from './suratoUmar/s4.png'
+import s5 from './suratoUmar/s5.png'
+import s6 from './suratoUmar/s6.png'
+import s7 from './suratoUmar/s7.png'
+import s8 from './suratoUmar/s8.png'
+import s9 from './suratoUmar/s9.png'
+const Contact = () => {
+  return (
+	 <div className='sm:p-[100px] p-[10px]'>
+    
+    <p className='text-[50px] my-[30px]'>Контакты</p>
+
+
+
+
+<section className='  items-start justify-between flex sm:flex-row flex-col w-full sm:h-[434px] my-[30px]'>
+  <article className='sm:w-[313px] sm:h-[270px] flex h-[239px] w-[313px] flex-col items-start justify-between p-[10px] '>
+<p className='text-[20px] font-[700]'>Центральный офис и склад</p>
+<div className='flex items-center gap-[20px]'>
+<img src={s1} alt="" />
+<p className='text-[#707070]'>+7 (3952) 648-139</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s2} alt="" />
+<p className='text-[#707070]'>postav.irk@mail.ru</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s3} alt="" />
+<p className='text-[#707070]'>+7(924) 626-33-40</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s4} alt="" />
+<p className='text-[#707070]'>г. Иркутск ул. Ракитная стр 4 корп 11</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s5} alt="" />
+<p className='text-[#707070]'>Пн-Пт с 9:00 до 18:00, сб с 09:00 до 14:00</p>
+</div>
+
+
+
+
+  </article>
+<img src={s6} alt="" />
+</section>
+<section className='  items-start justify-between flex sm:flex-row flex-col w-full sm:h-[434px] my-[30px]'>
+  <article className='sm:w-[313px] sm:h-[270px] flex h-[239px] w-[313px] flex-col items-start justify-between p-[10px] '>
+<p className='text-[20px] font-[700]'>Офис и склад на Трактовой</p>
+<div className='flex items-center gap-[20px]'>
+<img src={s1} alt="" />
+<p className='text-[#707070]'>+7 (3952) 648-139</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s2} alt="" />
+<p className='text-[#707070]'>postav.irk@mail.ru</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s3} alt="" />
+<p className='text-[#707070]'>+7(924) 626-33-40</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s4} alt="" />
+<p className='text-[#707070]'>г. Иркутск ул. Ракитная стр 4 корп 11</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s5} alt="" />
+<p className='text-[#707070]'>Пн-Пт с 9:00 до 18:00, сб с 09:00 до 14:00</p>
+</div>
+
+
+
+
+  </article>
+<img src={s6} alt="" />
+</section>
+<section className='  items-start justify-between flex sm:flex-row flex-col w-full sm:h-[434px] my-[30px]'>
+  <article className='sm:w-[313px] sm:h-[270px] flex h-[239px] w-[313px] flex-col items-start justify-between p-[10px] '>
+<p className='text-[20px] font-[700]'>Центральный офис и склад</p>
+<div className='flex items-center gap-[20px]'>
+<img src={s1} alt="" />
+<p className='text-[#707070]'>+7 (3952) 648-139</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s2} alt="" />
+<p className='text-[#707070]'>postav.irk@mail.ru</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s3} alt="" />
+<p className='text-[#707070]'>+7(924) 626-33-40</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s4} alt="" />
+<p className='text-[#707070]'>г. Иркутск ул. Ракитная стр 4 корп 11</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s5} alt="" />
+<p className='text-[#707070]'>Пн-Пт с 9:00 до 18:00, сб с 09:00 до 14:00</p>
+</div>
+
+
+
+
+  </article>
+<img src={s6} alt="" />
+</section>
+<section className='  items-start justify-between flex sm:flex-row flex-col w-full sm:h-[434px] my-[30px]'>
+  <article className='sm:w-[313px] sm:h-[270px] flex h-[239px] w-[313px] flex-col items-start justify-between p-[10px] '>
+<p className='text-[20px] font-[700]'>Офис и склад на Трактовой</p>
+<div className='flex items-center gap-[20px]'>
+<img src={s1} alt="" />
+<p className='text-[#707070]'>+7 (3952) 648-139</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s2} alt="" />
+<p className='text-[#707070]'>postav.irk@mail.ru</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s3} alt="" />
+<p className='text-[#707070]'>+7(924) 626-33-40</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s4} alt="" />
+<p className='text-[#707070]'>г. Иркутск ул. Ракитная стр 4 корп 11</p>
+</div>
+<div className='flex items-center gap-[20px]'>
+<img src={s5} alt="" />
+<p className='text-[#707070]'>Пн-Пт с 9:00 до 18:00, сб с 09:00 до 14:00</p>
+</div>
+
+
+
+
+  </article>
+<img src={s6} alt="" />
+</section>
+   </div>
+  )
+}
+
+export default Contact
